57b68e3f009adff04b39f336a1155ecbb65e799f
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-06-18 22:29:07 +0000

    SDK regeneration

--- src/multion/types/session_step_stream_chunk.py
+++ src/multion/types/session_step_stream_chunk.py
@@ -2,33 +2,65 @@
 
 from __future__ import annotations
 
+import datetime as dt
 import typing
 
 import typing_extensions
 
-from ..core.unchecked_base_model import UnionMetadata
-from .session_stream_chunk_event import SessionStreamChunkEvent
-from .session_stream_chunk_final_event import SessionStreamChunkFinalEvent
+from ..core.datetime_utils import serialize_datetime
+from ..core.pydantic_utilities import deep_union_pydantic_dicts, pydantic_v1
+from ..core.unchecked_base_model import UncheckedBaseModel, UnionMetadata
+from .session_stream_chunk_event_data import SessionStreamChunkEventData
+from .session_stream_chunk_final_event_data import SessionStreamChunkFinalEventData
 
 
-class SessionStepStreamChunk_Event(SessionStreamChunkEvent):
+class SessionStepStreamChunk_Event(UncheckedBaseModel):
+    data: SessionStreamChunkEventData
+    session_id: typing.Optional[str] = None
     type: typing.Literal["event"] = "event"
 
+    def json(self, **kwargs: typing.Any) -> str:
+        kwargs_with_defaults: typing.Any = {"by_alias": True, "exclude_unset": True, **kwargs}
+        return super().json(**kwargs_with_defaults)
+
+    def dict(self, **kwargs: typing.Any) -> typing.Dict[str, typing.Any]:
+        kwargs_with_defaults_exclude_unset: typing.Any = {"by_alias": True, "exclude_unset": True, **kwargs}
+        kwargs_with_defaults_exclude_none: typing.Any = {"by_alias": True, "exclude_none": True, **kwargs}
+
+        return deep_union_pydantic_dicts(
+            super().dict(**kwargs_with_defaults_exclude_unset), super().dict(**kwargs_with_defaults_exclude_none)
+        )
+
     class Config:
         frozen = True
         smart_union = True
-        allow_population_by_field_name = True
-        populate_by_name = True
+        extra = pydantic_v1.Extra.allow
+        json_encoders = {dt.datetime: serialize_datetime}
 
 
-class SessionStepStreamChunk_FinalEvent(SessionStreamChunkFinalEvent):
+class SessionStepStreamChunk_FinalEvent(UncheckedBaseModel):
+    data: SessionStreamChunkFinalEventData
+    session_id: typing.Optional[str] = None
+    screenshot: typing.Optional[str] = None
     type: typing.Literal["final_event"] = "final_event"
 
+    def json(self, **kwargs: typing.Any) -> str:
+        kwargs_with_defaults: typing.Any = {"by_alias": True, "exclude_unset": True, **kwargs}
+        return super().json(**kwargs_with_defaults)
+
+    def dict(self, **kwargs: typing.Any) -> typing.Dict[str, typing.Any]:
+        kwargs_with_defaults_exclude_unset: typing.Any = {"by_alias": True, "exclude_unset": True, **kwargs}
+        kwargs_with_defaults_exclude_none: typing.Any = {"by_alias": True, "exclude_none": True, **kwargs}
+
+        return deep_union_pydantic_dicts(
+            super().dict(**kwargs_with_defaults_exclude_unset), super().dict(**kwargs_with_defaults_exclude_none)
+        )
+
     class Config:
         frozen = True
         smart_union = True
-        allow_population_by_field_name = True
-        populate_by_name = True
+        extra = pydantic_v1.Extra.allow
+        json_encoders = {dt.datetime: serialize_datetime}
 
 
 SessionStepStreamChunk = typing_extensions.Annotated[
